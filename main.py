ed64c4c692b1e14031e8f0230f0fa6bb87c8dca4
Mastermindx33 <mastermindx33@gmail.com>
2021-11-03 01:46:23 +0530

    deploy @db.conn

--- main.py
+++ main.py
@@ -10,10 +10,61 @@ Item4 = 0
 
 # Create a new DB if dir is 404 @startup
 
-with sqlite3.connect('Users.db') as db:
-    c = db.cursor()
+with sqlite3.connect('Users.db') as conn:
+    conn.cursor()
     
-c.execute('CREATE TABLE IF NOT EXISTS user (username TEXT not NULL, password TEXT not NULL')
-db.commit()
-db.close()
+conn.execute('CREATE TABLE IF NOT EXISTS user (username TEXT not NULL, password TEXT not NULL')
+conn.commit()
+conn.close()
 
+# main
+class User:
+    def __init__(self, master):
+        # window
+        self.master = master
+        # some useful stuff
+        self.username = StringVar()
+        self.password = StringVar()
+        self.new_username = StringVar()
+        self.new_password = StringVar()
+        # widgets++ stuff
+        self.widgets()
+        
+        # Logins
+        def login(self):
+            # Establish DB connection
+            with sqlite3.connect('Users.db') as conn:
+                conn.cursor()
+                
+            # Fetch users
+            find_user = ('SELECT * FROM user WHERE username = ? and password = ?')
+            conn.execute(find_user, [self.username.get(), self.password.get()])
+            result = conn.fetchall()
+            if result:
+                self.logf.pack_forget()
+                self.head['text'] = "Welcome to Zola Cabs "+ self.username.get()
+                self.head.configure(fg="black")
+                self.head.pack(fill=X)
+                application = travel(root)
+            else:
+                msg.showerror('oops' ,'Username not found.')
+        
+        # user++
+        def new_user(self):
+            # Establish connection
+            with sqlite3.connect('User.db') as conn:
+                conn.cursor()
+                
+            # username Exists
+            find_user = ('SELECT * FROM user WHERE username = ?')
+            conn.execute(find_user, [self.username.get()])
+            if conn.fetchall():
+                msg.showerror('Error','username already taken')
+            else:
+                msg.showinfo('Success!','Account Created Succesfully')
+                self.log()
+            # Create new account
+            insert = 'INSERT INTO user(username, password) VALUES(?,?)'
+            conn.commit()
+            
+            # Frame Packing Methods
