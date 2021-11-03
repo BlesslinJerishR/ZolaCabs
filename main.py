5fbe895d8f84a79627d7bd5a3bca38b92b4690a2
Mastermindx33 <mastermindx33@gmail.com>
2021-11-03 23:41:30 +0530

    deploy @test

--- main.py
+++ main.py
@@ -92,4 +92,5 @@ class User:
             Label(self.logf, text = "Password : ", font = ('', 20), pady = 5, padx = 5).grid(sticky = W)
             Entry(self.logf, textvariable = self.username, bd = 5, font = ('', 15), show='*').grid(row = 1, column = 1)
             Button(self.logf, text = " Login ", bd = 3, font = ('', 15), padx = 5, pady = 5, command = self.login).grid()
-            Button()            
+            Button(self.logf,text = ' Register ', bd = 3, font = ('open sans', 15), padx =5, pady = 5, command = self.cr).grid(row = 2, column =1 )
+                        
