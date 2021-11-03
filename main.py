3a5284374fc853cfdd0a766a04563c8ad686305c
Mastermindx33 <mastermindx33@gmail.com>
2021-11-03 23:50:45 +0530

    deploy @today

--- main.py
+++ main.py
@@ -87,10 +87,13 @@ class User:
             self.head = Label(self.master, text = "Login Panel", font=('open sans', 30), pady = 10)
             self.head.pack()
             self.logf = Frame(self.master, padx = 10, pady = 10)
-            Label(self.logf, text = "Username : ", font = ('', 20), pady = 5, padx = 5).grid(sticky = W)
-            Entry(self.logf, textvariable = self.username, bd = 5, font = ('', 15)).grid(row = 0, column = 1)
-            Label(self.logf, text = "Password : ", font = ('', 20), pady = 5, padx = 5).grid(sticky = W)
-            Entry(self.logf, textvariable = self.username, bd = 5, font = ('', 15), show='*').grid(row = 1, column = 1)
-            Button(self.logf, text = " Login ", bd = 3, font = ('', 15), padx = 5, pady = 5, command = self.login).grid()
-            Button(self.logf,text = ' Register ', bd = 3, font = ('open sans', 15), padx =5, pady = 5, command = self.cr).grid(row = 2, column =1 )
-                        
+            Label(self.logf, text = "Username : ", font = ('open sans', 20), pady = 5, padx = 5).grid(sticky = W)
+            Entry(self.logf, textvariable = self.username, bd = 5, font = ('open sans', 15)).grid(row = 0, column = 1)
+            Label(self.logf, text = "Password : ", font = ('open sans', 20), pady = 5, padx = 5).grid(sticky = W)
+            Entry(self.logf, textvariable = self.username, bd = 5, font = ('open sans', 15), show='*').grid(row = 1, column = 1)
+            Button(self.logf, text = " Login ", bd = 3, font = ('open sans', 15), padx = 5, pady = 5, command = self.login).grid()
+            Button(self.logf,text = " Register ", bd = 3, font = ('open sans', 15), padx =5, pady = 5, command = self.cr).grid(row = 2, column =1 )
+            self.logf.pack()
+            
+            self.crf = Frame(self.master, padx = 10, pady = 10)
+            
